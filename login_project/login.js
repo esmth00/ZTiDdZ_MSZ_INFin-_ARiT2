006bc6cfae803d77ec49410e9f27f6d557cff6cf
vdi-terminal <vdi-terminal@EDU-WSB.local>
2022-11-27 10:11:38 +0100

    Naprawienie błędów

--- login_project/login.js
+++ login_project/login.js
@@ -7,10 +7,11 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "" && password === "") {
+    if (username === "admin" && password === "admin") {
         alert("You have successfully logged in.");
         location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
+        
     }
 })
